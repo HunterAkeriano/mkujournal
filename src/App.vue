644a8562fa4cfd2199a41a6eb7f1cda86760caf1
VUE Дмитрий Гулак <stajectgamer@gmail.com>
2024-11-21 22:17:06 +0200

    master: main scene

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import { RouterView } from 'vue-router'
 import TheHeader from '@/organisms/header/TheHeader.vue'
 import IconsManager from '@/organisms/icons-manager/IconsManager.vue'
+
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
 </script>
 
 <template>
--- src/organisms/header/TheHeader.vue
+++ src/organisms/header/TheHeader.vue
@@ -3,13 +3,20 @@ import BaseButton from '@/molecules/base-button-text/BaseButtonText.vue'
 import ModalContainer from '@/atom/modal-container/ModalContainer.vue'
 import { useToggleModal } from '@/molecules/toggle-modal/toggle-modal.js'
 import ModalRegistration from '@/organisms/modal-registration/ModalRegistration.vue'
+import { ref } from 'vue'
 
 const registrationModal = useToggleModal()
+const isOpenMenu = ref(false)
 
 function changeTypeModal() {
   //todo: add modal login
 
   registrationModal.close()
+}
+
+function openModalRegistration() {
+  registrationModal.open()
+  isOpenMenu.value = false
 }
 </script>
 
@@ -35,7 +42,42 @@ function changeTypeModal() {
             Реєстрація
           </BaseButton>
         </div>
+
+        <div
+          :class="{ 'header__mobile-btn_opened': isOpenMenu }"
+          class="header__mobile-btn"
+          @click="isOpenMenu = !isOpenMenu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
+    </div>
+
+    <div
+      :class="{ 'header__mobile-content_opened': isOpenMenu }"
+      class="header__mobile-content"
+    >
+      <div class="header__mobile-link">
+        <BaseButton clickable class="header__btn-item"> Вхід </BaseButton>
+        <BaseButton
+          clickable
+          class="header__btn-item"
+          @click="openModalRegistration"
+        >
+          Реєстрація
+        </BaseButton>
+        <BaseButton clickable class="header__btn-item"> Питання </BaseButton>
+      </div>
+
+      <div class="header__mobile-logo">
+        <img src="@/assets/img/logo.svg" alt="logo" />
+      </div>
+
+      <!--      <div class="header__mobile-bg">-->
+      <!--        <img src="@/assets/img/header-mobile.png" alt="header-mobile" />-->
+      <!--      </div>-->
     </div>
   </header>
 
