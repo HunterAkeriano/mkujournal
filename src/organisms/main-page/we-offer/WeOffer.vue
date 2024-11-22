e2b27becfc6a389f55b153a67ac648a8eb6e1a35
VUE Дмитрий Гулак <stajectgamer@gmail.com>
2024-11-22 10:22:30 +0200

    master: add swiper block

--- src/organisms/main-page/we-offer/WeOffer.vue
+++ src/organisms/main-page/we-offer/WeOffer.vue
@@ -1,15 +1,24 @@
 <script setup>
 import BaseCard from '@/atom/base-card/BaseCard.vue'
 import weOffer from '@/mixins/offer.js'
+
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+import 'swiper/scss'
+import 'swiper/scss/pagination'
 </script>
 
 <template>
-  <div class="we-offer">
-    <div class="container">
-      <div class="we-offer__title">
-        <h2>Ми пропонуємо:</h2>
-      </div>
+  <div class="we-offer__title">
+    <h2>Ми пропонуємо:</h2>
+  </div>
 
+  <div class="we-offer">
+    <div class="we-offer__bg">
+      <img src="@/assets/img/main/bg.png" alt="bg" />
+    </div>
+
+    <div class="container">
       <div class="we-offer__cards">
         <BaseCard
           v-for="item in weOffer"
@@ -26,6 +35,27 @@ import weOffer from '@/mixins/offer.js'
           </template>
         </BaseCard>
       </div>
+    </div>
+
+    <div class="we-offer__swiper">
+      <swiper
+        :centered-slides="true"
+        :loop="true"
+        space-between="20"
+        slides-per-view="1.5"
+      >
+        <swiper-slide v-for="item in weOffer" :key="item.id">
+          <BaseCard bg-color="white" class="we-offer__cards-item">
+            <template #header>
+              <h5>{{ item.name }}</h5>
+            </template>
+
+            <template #content>
+              <p>{{ item.content }}</p>
+            </template>
+          </BaseCard>
+        </swiper-slide>
+      </swiper>
     </div>
   </div>
 </template>
