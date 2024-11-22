6bf6e72c696a6a82f49355ba2446d8ac84eac80e
VUE Дмитрий Гулак <stajectgamer@gmail.com>
2024-11-22 10:52:40 +0200

    master: add arrow

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 import { RouterView } from 'vue-router'
 import TheHeader from '@/organisms/header/TheHeader.vue'
 import IconsManager from '@/organisms/icons-manager/IconsManager.vue'
+import BaseIcon from '@/molecules/base-icon/BaseIcon.vue'
+import { ref } from 'vue'
+
+const isActiveScroll = ref(false)
 
 const appHeight = () => {
   const doc = document.documentElement
@@ -9,6 +13,23 @@ const appHeight = () => {
 }
 window.addEventListener('resize', appHeight)
 appHeight()
+
+const handleScroll = () => {
+  if (window.scrollY > 20) {
+    isActiveScroll.value = true
+    return
+  }
+  isActiveScroll.value = false
+}
+
+window.addEventListener('scroll', handleScroll)
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+}
 </script>
 
 <template>
@@ -16,7 +37,17 @@ appHeight()
 
   <RouterView />
 
+  <div class="container">
+    <div
+      :class="{ 'scroll-top_visible': isActiveScroll }"
+      class="scroll-top"
+      ref="scrollTop"
+    >
+      <BaseIcon icon="icon-top" class="global-top" @click="scrollToTop" />
+    </div>
+  </div>
+
   <IconsManager />
 </template>
 
-<style scoped></style>
+<style></style>
