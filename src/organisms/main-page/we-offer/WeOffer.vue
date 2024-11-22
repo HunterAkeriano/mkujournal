5360ed611e573ae90fc51e01c9088bc7dce77cf2
VUE Дмитрий Гулак <stajectgamer@gmail.com>
2024-11-22 09:14:17 +0200

    master: add we offer

---
+++ src/organisms/main-page/we-offer/WeOffer.vue
@@ -0,0 +1,35 @@
+<script setup>
+import BaseCard from '@/atom/base-card/BaseCard.vue'
+import weOffer from '@/mixins/offer.js'
+</script>
+
+<template>
+  <div class="we-offer">
+    <div class="container">
+      <div class="we-offer__title">
+        <h2>Ми пропонуємо:</h2>
+      </div>
+
+      <div class="we-offer__cards">
+        <BaseCard
+          v-for="item in weOffer"
+          :key="item.id"
+          bg-color="white"
+          class="we-offer__cards-item"
+        >
+          <template #header>
+            <h5>{{ item.name }}</h5>
+          </template>
+
+          <template #content>
+            <p>{{ item.content }}</p>
+          </template>
+        </BaseCard>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'WeOffer';
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import MainScene from '@/organisms/main-page/main-scene/MainScene.vue'
+import WeOffer from '@/organisms/main-page/we-offer/WeOffer.vue'
 </script>
 
 <template>
   <main>
     <MainScene />
+
+    <WeOffer />
   </main>
 </template>
