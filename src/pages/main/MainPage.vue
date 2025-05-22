20a169936db33561b77a0ef549645f88fa1f7698
sergey-uhanov <uhanov.sergio@gmail.com>
2025-05-22 19:50:40 +0300

    sergey-dev: add input and Login page

--- src/pages/main/MainPage.vue
+++ src/pages/main/MainPage.vue
@@ -1,23 +1,5 @@
 <script setup lang="ts">
-import { AuthApi, useApi } from '@/shared'
 
-const api = useApi(AuthApi)
-
-async function getCatalog() {
-  try {
-    const { data } = await api.authLoginPost({
-      authLoginPostRequest: {
-        email: 'dimonmoy@meta.ua',
-        password: '12345678',
-      },
-    })
-    console.log(data)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-getCatalog()
 </script>
 
 <template>
